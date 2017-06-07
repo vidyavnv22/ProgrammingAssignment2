facece429db5c5b3c4e99a4791cc28085b488ab5
vidyavnv22 <vidyavnv22@gmail.com>
2017-06-07 16:39:04 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Couresra's R Programming Assignment 2 on Lexical Scoping. 
 
-## Write a short comment describing this function
+# ASSUMPTION: INPUT MATRIX IS ALWAYS INVERTIBLE. 
 
+# In the below function the following functions are defined. 
+# Get, Set Matrix and get Inverse, set Inverse (xirtam is inverse)
 makeCacheMatrix <- function(x = matrix()) {
-
+    xirtam <- NULL
+    set <- function(y) {
+      x <<- y
+      xirtam <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) xirtam <<- inverse
+    getinverse <- function() xirtam
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+# Getting inverse of the matrix from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    xirtam <- x$getinverse()
+    # Gets inverse after the first run, checks if it is the first run. If it is first run, 
+    # the inverse is computed using solve function, else inverted matrix is obtained from cache. 
+    if(!is.null(xirtam)) {
+      message("getting cached data")
+      return(xirtam)
+    }
+    data <- x$get()
+    # Matrix inversion is done using solve function in R
+    xirtam <- solve(data)
+    x$setinv(xirtam)
+    xirtam
 }
